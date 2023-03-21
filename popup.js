5c2b63abd58c55c73e2249297326d503beba82c5
solmar <solmar@dexeus.com>
2023-03-21 11:40:36 +0100

    first commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("github").addEventListener("click", function () {
+        window.open('https://github.com/masovi');
+    });
+    document.getElementById("contact-me").addEventListener("click", function () {
+        window.open('https://msoler.dev/');
+    });
+});
